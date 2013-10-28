9faa78097012c3517738a734f33ab9db21f8719b
maiordom <maiordom@yandex.ru>
2013-10-28 20:28:45 +0400

    update

--- casper.js
+++ casper.js
@@ -1,26 +1,42 @@
-var casper = require( 'casper' ).create();
+var casper = require( 'casper' ).create(),
+    url = 'index.html';
 
-casper.test.begin( 'Islider simple move', 1, function( test ) {
+casper.test.begin( 'Islider', 2, function( test ) {
+
+    casper.start( url );
+
+    test1( test );
+    test2( test );
+
+    casper.then( function() {
+        test.done();
+    });
+
+    casper.run();
+});
+
+function test1( test ) {
     var offsetOld, offsetNew;
-
-    casper.start( 'index.html' );
 
     casper.then( function() {
         var data = casper.evaluate( function() {
-            var el = $( '.islider__left' ).eq( 0 ),
-                elWidth = el.width(),
+            var el       = $( '.islider__left' ).eq( 0 ),
+                elWidth  = el.width(),
                 elHeight = el.height(),
                 offsetEl = el.offset();
 
             return {
-                left: offsetEl.left,
-                x: offsetEl.left + elWidth + 50,
-                y: offsetEl.top + elHeight / 2
+                startX: $( '.islider' ).eq( 0 ).offset().left,
+                elWidth: elWidth,
+                elHeight: elHeight,
+                old: offsetEl.left,
+                x: offsetEl.left,
+                y: offsetEl.top
             }
         });
 
-        offsetOld = data.left;
-        this.page.sendEvent( 'click', data.x, data.y );
+        offsetOld = data.old;
+        this.page.sendEvent( 'click', data.x + data.elWidth + 50, data.y + data.elHeight / 2 );
     });
 
     casper.wait( 1000 );
@@ -32,9 +48,39 @@ casper.test.begin( 'Islider simple move', 1, function( test ) {
     });
 
     casper.then( function() {
-        test.assert( offsetOld !== offsetNew );
-        test.done();
+        test.assert( offsetOld !== offsetNew, 'move left slider' );
+    });
+}
+
+function test2( test ) {
+    var pos;
+
+    casper.then( function() {
+        var data = this.evaluate( function() {
+            var el       = $( '.islider' ).eq( 1 ),
+                elWidth  = el.width(),
+                elHeight = el.height(),
+                offsetEl = el.offset();
+
+            return {
+                startX: el.offset().left,
+                elWidth: elWidth,
+                elHeight: elHeight,
+                x: offsetEl.left,
+                y: offsetEl.top
+            };
+        });
+
+        this.page.sendEvent( 'click', data.startX + 1, data.y );
     });
 
-    casper.run();
-});
+    casper.wait( 1000 );
+
+    casper.then( function() {
+        var pos = this.evaluate( function() {
+            return $( '.islider__left' ).eq( 1 ).position().left;
+        });
+
+        test.assert( pos === 0, 'move to start' );
+    });
+}
